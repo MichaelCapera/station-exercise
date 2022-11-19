959f6ccfc3306fbcfec44fdceb033c0fb5d7f5f1
Michael Capera <michaelcapera@gmail.com>
2022-11-18 20:38:29 -0500

    remove consoles

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
 let users = [];
 
 function addUser() {
-     console.log('add user');
     const name = document.querySelector('#userName').value;
- console.log("name ", name);
     const identificationNumber = document.querySelector('#identificationtNumber').value;
- console.log("identificationNumber ", identificationNumber);
     const bornDate = document.querySelector('#bornDate').value;
- console.log("bornDate ", bornDate);
     const userEmail = document.querySelector('#userEmail').value;
- console.log("userEmail ", userEmail);
     const cityResidence = document.querySelector('#cityResidence').value;
- console.log("cityResidence ", cityResidence);
     const cityOrigin = document.querySelector('#cityOrigin').value;
- console.log("cityOrigin ", cityOrigin);
     const author1 = document.querySelector('#author1').value;
- console.log("author1 ", author1);
     const song1 = document.querySelector('#song1').value;
- console.log("song1 ", song1);
     const author2 = document.querySelector('#author2').value;
- console.log("author2 ", author2);
     const song2 = document.querySelector('#song2').value;
- console.log("song2 ", song2);
     const author3 = document.querySelector('#author3').value;
- console.log("author3 ", author3);
     const song3 = document.querySelector('#song3').value;
- console.log("song3 ", song3);
 
     const user = {
         name,
@@ -42,22 +29,15 @@ function addUser() {
         song3
     }
 
-    
- console.log("user ", user);
-    
     users.push(user);
- console.log("users ", users);
 
-
+    alert('La información ha sido agregada');
 }
 
 function searchUser() {
 
 const index = document.querySelector('#userIndex').value;
-console.log("index  ", index );
-
 const userData = users[index];
-console.log("userData ", userData);
 
 const dataToShow = `
 <p>name: ${userData.name}</p><br>
